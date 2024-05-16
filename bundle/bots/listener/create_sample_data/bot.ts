d8628d1d9b1b3965babe96264d684a9b1c5960d8
humandad <55447225+humandad@users.noreply.github.com>
2024-05-16 14:34:28 -0400

    add account sample script

--- bundle/bots/listener/create_sample_data/bot.ts
+++ bundle/bots/listener/create_sample_data/bot.ts
@@ -5,4 +5,5 @@ export default function create_sample_data(bot: ListenerBotApi) {
 	bot.callBot("uesio/crm.default_lead_sources", {})
 	bot.callBot("uesio/crm.default_opportunity_stages", {})
 	bot.callBot("uesio/crm.create_sample_leads", {})
+	bot.callBot("uesio/crm.create_sample_accounts", {})
 }
